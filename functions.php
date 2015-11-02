bb139daaa4bfc73056ecb2af2fce8c85ab2819c8
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-11-02 13:20:24 +0200

    added success vs error

--- functions.php
+++ functions.php
@@ -1,103 +1,13 @@
 <?php
-    //loome AB ühenduse
-    /*  
-        // config_global.php
-        $servername = "";
-        $serverusername = "";
-        $serverpassword = "";
     
-    */
-    require_once("../config_global.php");
+	require_once("../config_global.php");
+	require_once("./user.class.php");
     $database = "if15_anniant";
-    
-    //paneme sessiooni serveris tööle, saaame kasutada SESSION[]
-    session_start();
-    
-    
-    function logInUser($email, $hash){
-        
-        // GLOBALS saab kätte kõik muutujad mis kasutusel
-        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
-        
-        $stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
-        $stmt->bind_param("ss", $email, $hash);
-        $stmt->bind_result($id_from_db, $email_from_db);
-        $stmt->execute();
-        if($stmt->fetch()){
-            echo "Kasutaja logis sisse id=".$id_from_db;
-            
-            // sessioon, salvestatakse serveris
-            $_SESSION['logged_in_user_id'] = $id_from_db;
-            $_SESSION['logged_in_user_email'] = $email_from_db;
-            
-            //suuname kasutaja teisele lehel
-            header("Location: data.php");
-            
-        }else{
-            echo "Wrong credentials!";
-        }
-        $stmt->close();
-        
-        $mysqli->close();
-        
-    }
-    
-    
-    function createUser($create_email, $hash){
-        
-        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
-        $stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
-        $stmt->bind_param("ss", $create_email, $hash);
-        $stmt->execute();
-        $stmt->close();
-        
-        $mysqli->close();
-        
-    }
 	
-	function createCarPlate($plate, $car_color){
-		
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
-		$stmt = $mysqli->prepare("INSERT INTO car_plates (user_id, number_plate, color) VALUES (?,?,?)");
-		$stmt->bind_param("iss", $_SESSION['logged_in_user_id'], $plate, $car_color);
-		
-		$message="";
-		
-		//kui õnnestub siis tõene kui viga siis else
-		if ($stmt->execute()){
-			//õnnestus
-			$message="edukalt andmebaasi salvestatud";
-		}
-		
-		$stmt->close();
-		$mysqli->close();
-		
-		return $message;
-	}
+	session_start();
 	
-	function getAllData(){
-		
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
-		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+	$mysqli = new mysqli($servername, $serverusername, $serverpassword, $database);
 	
-	        /*$row_nr = 0;
-        // iga rea kohta mis on ab'is teeme midagi
-        while($stmt->fetch()){
-            //saime andmed kätte
-            echo $row_nr." ".$number_plate_from_db." <br>";
-            $row_nr++;
-        }*/
-        
-        // iga rea kohta mis on ab'is teeme midagi
-        while($stmt->fetch()){
-            //saime andmed kätte
-            echo($user_id_from_db);
-            //? kuidas saada massiivi - SIIT JÄTKAME
-        }
-        
-        $stmt->close();
-        $mysqli->close();
+	$User = new User($mysqli);
 	
-	}
-	
- ?>
+?>
--- user.class.php
+++ user.class.php
@@ -1,18 +1,91 @@
 <?php
-$user1=new User();
-$user2=new User();
-
 class User {
 	
-	//user.class.php
+	private $connection;
 	
 	//see funk. käivitus kui tekitame uue instantsi
 	//new user 
-	function __construct(){
+	function __construct($mysqli){
 		
+		//$this on see klass e User
+		//-> connection on klassi muutuja 
+		$this->connection=$mysqli;
 		
 	}
 	
-	
+	function logInUser($email, $hash){
+        
+        $stmt = $this->connection->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+        $stmt->bind_param("ss", $email, $hash);
+        $stmt->bind_result($id_from_db, $email_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            echo "Kasutaja logis sisse id=".$id_from_db;
+            
+            // sessioon, salvestatakse serveris
+            $_SESSION['logged_in_user_id'] = $id_from_db;
+            $_SESSION['logged_in_user_email'] = $email_from_db;
+            
+            //suuname kasutaja teisele lehel
+            header("Location: data.php");
+            
+        }else{
+            echo "Wrong credentials!";
+        }
+        $stmt->close();
+        
+    }
+    
+    
+    function createUser($create_email, $hash){
+		
+		//objekt kus tagastame errori (id, message) või successi (message)
+		$response= new StdClass();
+		
+		//kontrollime kas on juba olemas sellise emailiga kasutaja
+		$stmt = $this->connection->prepare("SELECT id FROM user_sample WHERE email=?");
+		$stmt->bind_param("s", $create_email);
+		$stmt->bind_result($id);
+        $stmt->execute();	
+        
+		//kas saime rea andmeid
+		if($stmt->fetch()){
+			
+			//email on juba olemas
+			$error=new StdClass();
+			$error->id = 0;
+			$error->message = "Email on juba kasutusel.";
+			
+			$response->error=$error;
+			
+			//pärast return käsku ei vaadata funki edasi
+			return $response;
+		}
+		
+		//siia olen jõudnud siis kui emaili ei olnud 
+		
+        $stmt = $this->connection->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+        $stmt->bind_param("ss", $create_email, $hash);
+        if ($stmt->execute()){
+			//sisestamine õnnestus
+			$success=new StdClass();
+			$success->message= "Kasutaja edukalt loodud";
+			
+			$response->success=$success;
+			
+		}
+		else{
+			//ei õnnestunud
+			$error=new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi läks katki.";
+			
+			$response->error=$error;			
+		}
+        $stmt->close();
+		
+		return $response;
+        
+    }
 }
 ?> 
